f07e4eefdb95d76c0f8510b31d0a6963b61693a7
DianaI <andreea.diana.ion@gmail.com>
2021-02-01 18:18:22 +0200

    added solution to source control

---
+++ TicTacToe/Model.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace TicTacToe
+{
+    public class Model
+    {
+        public int click = 1;
+
+        public string topLeft;
+        public string topCenter;
+        public string topRight;
+
+        public string middleLeft;
+        public string middleCenter;
+        public string middleRight;
+
+        public string bottomLeft;
+        public string bottomCenter;
+        public string bottomRight;
+
+
+        public string BtnClick(string content)
+        {
+            if (string.IsNullOrEmpty(content))
+            {
+                if (click % 2 == 0)
+                {
+                    content = "0";
+                    click++;
+                }
+                else
+                {
+                    content = "X";
+                    click++;
+                }
+            }
+            return content;
+        }
+        public void checkVictory()
+        {
+            CheckVictoryConditionsRow();
+            CheckVictoryConditionsColumn();
+            CheckVictoryConditionsDiagonally();
+        }
+
+        private void CheckVictoryConditionsRow()
+        {
+            if ((topLeft == topCenter && topRight == topCenter) && topLeft !=null)
+            {
+                if (topLeft == "X")
+                {
+                    MessageBox.Show("X won");
+                }
+                else
+                {
+                    MessageBox.Show("0 won");
+                }
+            }
+            if ((middleLeft == middleCenter && middleCenter == middleRight) && middleLeft != null)
+            {
+
+                if (middleLeft == "X")
+                {
+                    MessageBox.Show("X won");
+                }
+                else
+                {
+                    MessageBox.Show("0 won");
+                }
+            }
+            if ((bottomLeft == bottomCenter && bottomRight == bottomCenter) && bottomLeft != null)
+            {
+
+                if (bottomLeft == "X")
+                {
+                    MessageBox.Show("X won");
+                }
+                else
+                {
+                    MessageBox.Show("0 won");
+                }
+            }
+        }
+        private void CheckVictoryConditionsColumn()
+        {
+            if ((topLeft == middleLeft && bottomLeft == middleLeft) && topLeft != null)
+            {
+
+                if (topLeft == "X")
+                {
+                    MessageBox.Show("X won");
+                }
+                else
+                {
+                    MessageBox.Show("0 won");
+                }
+            }
+            if ((topCenter ==  middleCenter && bottomCenter == middleCenter) && topCenter!= null)
+            {
+
+                if (topCenter == "X")
+                {
+                    MessageBox.Show("X won");
+                }
+                else
+                {
+                    MessageBox.Show("0 won");
+                }
+            }
+            if ((topRight == middleRight&& bottomRight == middleRight) && topRight!= null)
+            {
+
+                if (topRight == "X")
+                {
+                    MessageBox.Show("X won");
+                }
+                else
+                {
+                    MessageBox.Show("0 won");
+                }
+            }
+        }
+        private void CheckVictoryConditionsDiagonally()
+        {
+            if ((topLeft == middleCenter && bottomRight == middleCenter) && topLeft != null)
+            {
+
+                if (topLeft == "X")
+                {
+                    MessageBox.Show("X won");
+                }
+                else
+                {
+                    MessageBox.Show("0 won");
+                }
+            }
+            if ((topRight == middleCenter&& bottomLeft == middleCenter) && topRight != null)
+            {
+
+                if (topRight == "X")
+                {
+                    MessageBox.Show("X won");
+                }
+                else
+                {
+                    MessageBox.Show("0 won");
+                }
+            }
+        }
+        public void Reset()
+        {
+         click = 1;
+
+         topLeft=null;
+         topCenter = null;
+         topRight = null;
+
+         middleLeft = null;
+         middleCenter = null;
+         middleRight = null;
+
+         bottomLeft = null;
+         bottomCenter = null;
+         bottomRight = null;
+    }
+
+
+    }
+    }
+
---
+++ TicTacToe/ViewModel.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+
+namespace TicTacToe
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        Model model ;
+
+        public Model DataContext { get; private set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void RaisePropertyChange(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public ViewModel()
+        {
+             model = new Model();
+            Btn1Command = new CommandHandler(Btn1ClickTicTac, canExecute);
+            Btn2Command = new CommandHandler(Btn2ClickTicTac, canExecute);
+            Btn3Command = new CommandHandler(Btn3ClickTicTac, canExecute);
+            Btn4Command = new CommandHandler(Btn4ClickTicTac, canExecute);
+            Btn5Command = new CommandHandler(Btn5ClickTicTac, canExecute);
+            Btn6Command = new CommandHandler(Btn6ClickTicTac, canExecute);
+            Btn7Command = new CommandHandler(Btn7ClickTicTac, canExecute);
+            Btn8Command = new CommandHandler(Btn8ClickTicTac, canExecute);
+            Btn9Command = new CommandHandler(Btn9ClickTicTac, canExecute);
+            ResetCommand = new CommandHandler(Reset, canExecute);
+        }
+        public ICommand ResetCommand { get; set; }
+        public string Btn1 { get; set; }
+        public ICommand Btn1Command { get; private set; }
+
+        public string Btn2 { get; set; }
+        public ICommand Btn2Command { get; private set; }
+
+        public string Btn3 { get; set; }
+        public ICommand Btn3Command { get; private set; }
+
+        public string Btn4 { get; set; }
+        public ICommand Btn4Command { get; private set; }
+
+        public string Btn5 { get; set; }
+        public ICommand Btn5Command { get; private set; }
+
+        public string Btn6 { get; set; }
+        public ICommand Btn6Command { get; private set; }
+
+        public string Btn7 { get; set; }
+        public ICommand Btn7Command { get; private set; }
+
+        public string Btn8 { get; set; }
+        public ICommand Btn8Command { get; private set; }
+
+        public string Btn9 { get; set; }
+        public ICommand Btn9Command { get; private set; }
+
+        private void Btn1ClickTicTac()
+        {
+            var content=model.BtnClick(Btn1);
+            Btn1 = content;
+            RaisePropertyChange(nameof(Btn1));
+            model.topLeft = content;
+            model.checkVictory();
+        }
+
+        private void Btn2ClickTicTac()
+        {
+            var content = model.BtnClick(Btn2);
+            Btn2 = content;
+            RaisePropertyChange(nameof(Btn2));
+            model.topCenter = content;
+            model.checkVictory();
+        }
+
+        private void Btn3ClickTicTac()
+        {
+            var content = model.BtnClick(Btn3);
+            Btn3 = content;
+            RaisePropertyChange(nameof(Btn3));
+            model.topRight = content;
+            model.checkVictory();
+        }
+
+        private void Btn4ClickTicTac()
+        {
+            var content = model.BtnClick(Btn4);
+            Btn4 = content;
+            RaisePropertyChange(nameof(Btn4));
+            model.middleLeft = content;
+            model.checkVictory();
+        }
+
+        private void Btn5ClickTicTac()
+        {
+            var content = model.BtnClick(Btn5);
+            Btn5 = content;
+            RaisePropertyChange(nameof(Btn5));
+            model.middleCenter = content;
+            model.checkVictory();
+        }
+
+        private void Btn6ClickTicTac()
+        {
+            var content = model.BtnClick(Btn6);
+            Btn6 = content;
+            RaisePropertyChange(nameof(Btn6));
+            model.middleRight = content;
+            model.checkVictory();
+        }
+
+        private void Btn7ClickTicTac()
+        {
+            var content = model.BtnClick(Btn7);
+            Btn7 = content;
+            RaisePropertyChange(nameof(Btn7));
+            model.bottomLeft = content;
+            model.checkVictory();
+        }
+
+        private void Btn8ClickTicTac()
+        {
+            var content = model.BtnClick(Btn8);
+            Btn8 = content;
+            RaisePropertyChange(nameof(Btn8));
+            model.bottomCenter = content;
+            model.checkVictory();
+        }
+
+        private void Btn9ClickTicTac()
+        {
+            var content = model.BtnClick(Btn9);
+            Btn9 = content;
+            RaisePropertyChange(nameof(Btn9));
+            model.bottomRight = content;
+            model.checkVictory();
+        }
+
+        public void Reset()
+        {
+            Btn1 = null;
+            RaisePropertyChange(nameof(Btn1));
+            Btn2 = null;
+            RaisePropertyChange(nameof(Btn2));
+            Btn3 = null;
+            RaisePropertyChange(nameof(Btn3));
+            Btn4 = null;
+            RaisePropertyChange(nameof(Btn4));
+            Btn5 = null;
+            RaisePropertyChange(nameof(Btn5));
+            Btn6= null;
+            RaisePropertyChange(nameof(Btn6));
+            Btn7 = null;
+            RaisePropertyChange(nameof(Btn7));
+            Btn8 = null;
+            RaisePropertyChange(nameof(Btn8));
+            Btn9 = null;
+            RaisePropertyChange(nameof(Btn9));
+
+            model.Reset();
+        }
+
+        public bool canExecute()
+        {
+           
+            return true;
+
+        }
+    }
+}
